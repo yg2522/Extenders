e90479f4e9af5179ad93d22b45a543daa24469a3
yg2522 <yg2522@yahoo.com>
2016-09-22 14:11:21 -0400

    Update ObjectExtenders.cs

--- Extensions/ObjectExtenders.cs
+++ Extensions/ObjectExtenders.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace loctool.Utility.Extenders
+namespace Utility.Extenders
 {
     public static class ObjectExtenders
     {
