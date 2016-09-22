7bab46eead2df8904a6bb091b53cf07fc6e052fc
yg2522 <yg2522@yahoo.com>
2016-09-22 14:11:44 -0400

    Update StringExtenders.cs

--- Extensions/StringExtenders.cs
+++ Extensions/StringExtenders.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace loctool.Utility.Extenders
+namespace Utility.Extenders
 {
     public static class StringExtenders
     {
