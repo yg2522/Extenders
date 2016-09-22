e291becfe2fc706cd0bc1d1238bf8be118025a1e
yg2522 <yg2522@yahoo.com>
2016-09-22 14:10:45 -0400

    Update ExceptionExtenders.cs

--- Extensions/ExceptionExtenders.cs
+++ Extensions/ExceptionExtenders.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace loctool.Utility.Extenders
+namespace Utility.Extenders
 {
     public static class ExceptionExtenders
     {
