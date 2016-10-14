da42105122eb24567bf4a411ddc5f9f7847afb65
yg2522 <yg2522@yahoo.com>
2016-10-14 10:15:37 -0400

    Add files via upload
    Adding Datetime Extenders.  This one converts from one timezone to another based on Timezone enum provided by T4 file

---
+++ Extensions/DateTimeExtenders.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace Utility.Extenders
+{
+    public static class DateTimeExtenders
+    {
+        public static DateTime ConvertToTimeZone(this DateTime originaltime, TimezoneEnum timezoneid)
+        {
+            return originaltime.ConvertToTimeZone(timezoneid.GetDescription());
+        }
+
+        public static DateTime ConvertToTimeZone(this DateTime originaltime, string timezoneid)
+        {
+            try
+            {
+                TimeZone timezone = TimeZone.CurrentTimeZone;
+                DateTime lastlogin = timezone.ToUniversalTime(originaltime);
+                TimeZoneInfo timezoneinfo = null;
+
+                if (string.IsNullOrEmpty(timezoneid))
+                    return originaltime;
+                else
+                {
+                    timezoneinfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneid);
+                    return TimeZoneInfo.ConvertTimeFromUtc(lastlogin, timezoneinfo);
+                }
+            }
+            catch
+            {
+                return originaltime;
+            }
+        }
+    }
+}
